92e95c307b76e7c4ae5e39b7309feb1a84cf2ece
Roger Bagué <rogerbague@gmail.com>
2021-01-12 16:23:32 +0100

    modularize client js files
    move terminal and socket setup to a different class so it can be reused,
    andleave index file only with browser specific code to make it work

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,65 +1,24 @@
-import { Terminal } from 'xterm'
-import { FitAddon } from 'xterm-addon-fit'
-import { WebLinksAddon } from 'xterm-addon-web-links'
-import { io } from 'socket.io-client'
+import WebSSH from './webssh.js'
 
 import './style.css'
-import 'xterm/css/xterm.css'
 
-const params = new URLSearchParams(window.location.search)
-const options = {
-  query: {
-    host: params.get('host')
-  },
-  auth: {
+window.addEventListener('load', function setup () {
+  window.removeEventListener('load', setup)
+
+  const params = new URLSearchParams(window.location.search)
+  const webssh = new WebSSH(document.getElementById('terminal'), {
+    url: 'http://localhost:2222',
+    host: params.get('host'),
+    port: params.get('port'),
     username: params.get('user'),
     password: params.get('passwd')
-  }
-}
-if (params.has('port')) {
-  options.query.port = params.get('port')
-}
+  })
 
-const socket = io('http://localhost:2222', { path: '/socket', ...options })
+  window.addEventListener('resize', webssh.resize)
+  window.addEventListener('beforeunload', function cleanup () {
+    webssh.dispose()
 
-const fitAddon = new FitAddon()
-const terminal = new Terminal({
-  cursorBlink: true,
-  bellStyle: 'sound',
-  convertEol: true,
-  screenReaderMode: false,
-  tabStopWidth: 2
+    window.removeEventListener('resize', webssh.resize)
+    window.removeEventListener('beforeunload', cleanup)
+  })
 })
-terminal.loadAddon(new WebLinksAddon())
-terminal.loadAddon(fitAddon)
-terminal.open(document.getElementById('terminal'))
-terminal.focus()
-fitAddon.fit()
-
-terminal.onData(data => socket.emit('data', data))
-socket.on('data', data => terminal.write(data))
-
-socket.on('connect', resize)
-socket.on('connect_error', () => console.error('could not connect with the ssh server'))
-socket.on('error', message => {
-  terminal.reset()
-  terminal.writeln(`\u001b[31m${message}\u001b[0m`)
-})
-socket.on('disconnect', reason => {
-  if (/server/i.test(reason)) {
-    console.error('connection closed by ssh server')
-  }
-  socket.io.reconnection(false)
-  terminal.dispose()
-})
-
-window.addEventListener('resize', resize)
-window.onunload = function () {
-  if (terminal) terminal.dispose()
-  if (socket) socket.disconnect()
-}
-
-function resize () {
-  socket.emit('resize', { rows: terminal.rows, cols: terminal.cols })
-  fitAddon.fit()
-}
---
+++ packages/client/src/webssh.js
@@ -0,0 +1,87 @@
+import { Terminal } from 'xterm'
+import { FitAddon } from 'xterm-addon-fit'
+import { WebLinksAddon } from 'xterm-addon-web-links'
+import { io } from 'socket.io-client'
+
+import 'xterm/css/xterm.css'
+
+export default class WebSSH {
+  constructor (element, options) {
+    this.prepareTerminal(element)
+    this.connectSocket(options)
+    this.addListeners()
+  }
+
+  prepareTerminal (element) {
+    this.terminal = new Terminal({
+      cursorBlink: true,
+      bellStyle: 'sound',
+      convertEol: true,
+      screenReaderMode: false,
+      tabStopWidth: 2
+    })
+    this.terminal.loadAddon(new WebLinksAddon())
+    this.terminal.loadAddon(this.fitAddon = new FitAddon())
+    this.terminal.open(element)
+    this.terminal.focus()
+    this.fitAddon.fit()
+  }
+
+  connectSocket ({ url, ...config }) {
+    const auth = { username: config.username, password: config.password }
+    const query = { host: config.host }
+    if (config.port) query.port = config.port // optional parameter
+
+    this.socket = io(url, { path: '/socket', auth, query })
+  }
+
+  addListeners () {
+    if (!this.terminal || !this.socket) return
+
+    this.terminal.onData(data => this.socket.emit('data', data))
+    this.socket.on('data', data => this.terminal.write(data))
+
+    this.socket.on('connect', this.resize)
+    this.socket.on('connect_error', () => console.error('could not connect with the ssh server'))
+    this.socket.on('error', message => {
+      this.terminal.reset()
+      this.terminal.writeln(`\u001b[31m${message}\u001b[0m`)
+    })
+    this.socket.on('disconnect', reason => {
+      this.dispose()
+
+      if (/server/i.test(reason)) {
+        console.error('connection closed by ssh server')
+      }
+    })
+  }
+
+  resize () {
+    if (this.terminal) {
+      if (this.fitAddon) {
+        this.fitAddon.fit()
+      }
+
+      if (this.socket) {
+        this.socket.emit('resize', {
+          rows: this.terminal.rows,
+          cols: this.terminal.cols
+        })
+      }
+    }
+  }
+
+  dispose () {
+    if (this.terminal) {
+      this.terminal.dispose()
+      this.terminal = null
+      this.fitAddon = null
+    }
+
+    if (this.socket) {
+      this.socket.io.reconnection(false)
+      this.socket.disconnect()
+      this.socket = null
+    }
+  }
+}
